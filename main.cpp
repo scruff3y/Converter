3492ab66c48e41e2fbb4655074710cad3d50f408
Antony <southworthy@gmail.com>
2016-01-24 18:36:12 +1300

    added "/?" help command
    also doesn't enter main loop when called from command line, instead performs conversion and quits.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,27 @@
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
-		convert::parseAndConvert(std::string(argv[1]));
+		convert::stdLowercase(std::string(argv[1]));
+		if (convert::AreSameString(argv[1], "/?")) {
+			std::cout << "\nUsage: <quantity> <unit> OR <quantity><unit>" << std::endl;
+		}
+		else {
+			std::cout << "\n";
+			convert::parseAndConvert(std::string(argv[1]));
+		}
+		return 0;
 	}
 	else if (argc == 3) {
 		std::string cmdLineInput(argv[1]);
 		cmdLineInput += argv[2];
+		convert::stdLowercase(cmdLineInput);
 		convert::parseAndConvert(cmdLineInput);
+		return 0;
 	}
 	else {
-		std::cout << "unrecognised command-line input" << std::endl;
-	}
-
-	while (true) {
-		std::cout << "\nconvert:>";
-		std::string input;
-		std::getline(std::cin, input);
-
-		if (convert::AreSameString(input, std::string("quit"))) break;
-
-		convert::parseAndConvert(input);
+		std::cout << "Converter by scruff3y. Vist me on GitHub at\n\n\thttps://github.com/scruff3y" << std::endl;
+		convert::convertLoop();
 	}
 
 	return 0;
 }
-
