ff516a81a73222f63783383c01b453aa162db4fe
Antony <southworthy@gmail.com>
2016-01-26 16:10:25 +1300

    moved commands to parseAndConvert, added "about"

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char *argv[]) {
 	if (argc == 2) {
-		convert::stdLowercase(std::string(argv[1]));
+		convert::makeStdStringLowercase(std::string(argv[1]));
 		if (convert::AreSameString(argv[1], "/?")) {
-			std::cout << "\nUsage: <quantity> <unit> OR <quantity><unit>" << std::endl;
+			std::cout << "\nUsage: <quantity><unit>" << std::endl;
 		}
 		else {
 			std::cout << "\n";
@@ -15,13 +15,22 @@ int main(int argc, char *argv[]) {
 	else if (argc == 3) {
 		std::string cmdLineInput(argv[1]);
 		cmdLineInput += argv[2];
-		convert::stdLowercase(cmdLineInput);
+		convert::makeStdStringLowercase(cmdLineInput);
 		convert::parseAndConvert(cmdLineInput);
 		return 0;
 	}
 	else {
-		std::cout << "Converter by scruff3y. Vist me on GitHub at\n\n\thttps://github.com/scruff3y" << std::endl;
-		convert::convertLoop();
+		while (true) {
+			std::cout << "\nconvert:>";
+			std::string input;
+			std::getline(std::cin, input);
+
+			convert::makeStdStringLowercase(input);
+
+			if (convert::AreSameString(input, std::string("quit"))) break;
+
+			convert::parseAndConvert(input);
+		}
 	}
 
 	return 0;
